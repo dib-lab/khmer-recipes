30027d20901f7bcb874e4404932ca12d4872bcfb
C. Titus Brown <titus@idyll.org>
2015-06-26 07:10:39 -0700

    add set -x to env

--- env.sh
+++ env.sh
@@ -1,3 +1,8 @@
-. ../../ipy7/bin/activate
+#! /bin/bash
+set -x
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+PYTHON_VIRTUALENV="$DIR/../ipy7/bin"
+. ${PYTHON_VIRTUALENV}/activate
 set -o nounset
 set -o errexit
